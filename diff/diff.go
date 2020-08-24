725e241b69f7753fcd44d3a3c6f3f61979853c45
ari <arithran@gmail.com>
2020-08-24 13:37:58 -0700

    fix: better formatting

--- diff/diff.go
+++ diff/diff.go
@@ -21,8 +21,8 @@ import (
 )
 
 const curlTemplate = `
-Row: {{.Row}}
-
+Testing Row: {{.Row}}
+===============
 Before:
 curl --location --request {{ .Before.Method }} '{{ .Before.Path }}' \{{range $k, $v := .Before.Headers}}
 --header '{{$k}}: {{$v}}'{{end}}
@@ -31,6 +31,7 @@ After:
 curl --location --request {{ .After.Method }} '{{ .After.Path }}' \{{range $k, $v := .After.Headers}}
 --header '{{$k}}: {{$v}}'{{end}}
 
+Result:
 `
 
 const (
