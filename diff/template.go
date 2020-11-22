a077985353c51f900be48b4b784b1f9881bb8ed6
MohamedAmrAbuelhassan <mohamed.amr@wizeline.com>
2020-11-21 23:17:11 -0600

    fix: remove extra spaces from curl template

--- diff/template.go
+++ diff/template.go
@@ -7,13 +7,13 @@ Testing Row: {{.Row}}
 ===============
 Before:
 curl --location --request {{ .Before.Method }} '{{ .Before.Path }}' \{{range $k, $v := .Before.Headers}}
---header '{{$k}}: {{$v}}' \{{end}} {{if ne (len .Before.Body) 0}}
---data-raw '{{ .Before.Body }}' {{end}}
+--header '{{$k}}: {{$v}}' \{{end}}{{if ne (len .Before.Body) 0}}
+--data-raw '{{ .Before.Body }}'{{end}}
 
 After:
 curl --location --request {{ .After.Method }} '{{ .After.Path }}' \{{range $k, $v := .After.Headers}}
---header '{{$k}}: {{$v}}' \{{end}} {{if ne (len .After.Body) 0}}
---data-raw '{{ .After.Body }}' {{end}}
+--header '{{$k}}: {{$v}}' \{{end}}{{if ne (len .After.Body) 0}}
+--data-raw '{{ .After.Body }}'{{end}}
 
 `
 
